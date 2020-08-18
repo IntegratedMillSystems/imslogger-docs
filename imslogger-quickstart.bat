e3d4f7164c36b646aba8c91d5a1286a236918483
imslogger <imslogger@gmail.com>
2020-08-18 14:24:37 -0400

    Add quickstart scripts

---
+++ imslogger-quickstart.bat
@@ -0,0 +1,8 @@
+git clone https://github.com/imslogger/imslogger-server %1
+cd %1
+git rm -r scripts
+git submodule add https://github.com/imslogger/imslogger-cmd scripts
+cd scripts
+git rm -r imslogger
+git submodule add https://github.com/imslogger/imslogger-py imslogger
+cd ..
