ed04089136182ec07677f418ba6b4a9663f54ae5
J. Simon Richard <jsimonrichard@pop-os.localdomain>
2020-12-15 13:30:39 -0500

    Switch to React

--- imslogger-quickstart.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-git clone https://github.com/imslogger/imslogger-server %1
-cd %1
-git rm -r scripts
-git submodule add https://github.com/imslogger/imslogger-cmd scripts
-cd scripts
-git rm -r imslogger
-git submodule add https://github.com/imslogger/imslogger-py imslogger
--- imslogger-quickstart.sh
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/imslogger/imslogger-server $1
-cd $1
-git rm -r scripts
-git submodule add https://github.com/imslogger/imslogger-cmd scripts
-cd scripts
-git rm -r imslogger
-git submodule add https://github.com/imslogger/imslogger-py imslogger
-cd ..
