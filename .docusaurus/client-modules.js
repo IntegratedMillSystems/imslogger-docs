99a0319f61294799287cab06eca53b8221316f3f
jsimonrichard <jsimonrichard@gmail.com>
2021-05-25 14:43:26 -0400

    :fire: remove ignored files

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/jsimonrichard/Desktop/IMS/IMSLogger/imslogger-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/jsimonrichard/Desktop/IMS/IMSLogger/imslogger-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/jsimonrichard/Desktop/IMS/IMSLogger/imslogger-docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/jsimonrichard/Desktop/IMS/IMSLogger/imslogger-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/jsimonrichard/Desktop/IMS/IMSLogger/imslogger-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/jsimonrichard/Desktop/IMS/IMSLogger/imslogger-docs/src/css/custom.css"),
-];
